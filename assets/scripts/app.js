0fe98da09050668d6cec28edf9d3b01fac9b6e55
cmd08010 <colleendunion@gmail.com>
2021-03-04 08:17:56 -0500

    Add scripts for campaign and user

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,12 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const userEvents = require('./user/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+
+  $('#sign-in').on('click')
 })
